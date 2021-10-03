45eeca99fbe794e8361ee6ebb3b111c1fba5491d
Thanh Huynh <huynhbathanh@gmail.com>
2021-10-03 15:00:55 +0700

    Add Account context

---
+++ test/support/fixtures/account_fixtures.ex
@@ -0,0 +1,21 @@
+defmodule ProgrammingPhoenix.AccountFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `ProgrammingPhoenix.Account` context.
+  """
+
+  @doc """
+  Generate a user.
+  """
+  def user_fixture(attrs \\ %{}) do
+    {:ok, user} =
+      attrs
+      |> Enum.into(%{
+        name: "some name",
+        username: "some username"
+      })
+      |> ProgrammingPhoenix.Account.create_user()
+
+    user
+  end
+end
